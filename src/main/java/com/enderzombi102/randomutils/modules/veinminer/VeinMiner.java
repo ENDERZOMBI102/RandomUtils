19826176437817e64358eb7b38781f0f43957b54
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-10-23 00:14:05 +0200

    added basic template

--- src/main/java/com/enderzombi102/randomutils/modules/veinminer/VeinMiner.java
+++ src/main/java/com/enderzombi102/randomutils/modules/veinminer/VeinMiner.java
@@ -1,11 +1,32 @@
 package com.enderzombi102.randomutils.modules.veinminer;
 
 import com.enderzombi102.randomutils.modules.ModuleBase;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+
+import java.util.HashMap;
+import java.util.UUID;
 
 public class VeinMiner extends ModuleBase {
 
+	private HashMap<UUID, VeinMinerSettings> settings = new HashMap<>();
+	private VeinMinerListener listener;
+
+	public VeinMiner() {
+		this.listener = new VeinMinerListener();
+	}
+
 	@Override
 	public void stop() {
+		HandlerList.unregisterAll(this.listener);
+	}
 
+	private class VeinMinerListener implements Listener {
+		@EventHandler
+		public void onBlockBreak(BlockBreakEvent evt) {
+
+		}
 	}
 }
