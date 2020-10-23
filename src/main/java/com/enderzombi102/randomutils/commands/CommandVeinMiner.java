85a0bdbf5856d0010a9871e6d16cdd7af636ce1f
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-10-23 12:00:08 +0200

    actually implemented VeinMiner

--- src/main/java/com/enderzombi102/randomutils/commands/CommandVeinMiner.java
+++ src/main/java/com/enderzombi102/randomutils/commands/CommandVeinMiner.java
@@ -1,19 +1,30 @@
 package com.enderzombi102.randomutils.commands;
 
+import com.enderzombi102.randomutils.modules.veinminer.VeinMiner;
+import com.enderzombi102.randomutils.modules.veinminer.VeinMinerSettings;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class CommandVeinMiner implements TabExecutor {
 
 	@Override
 	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-		return false;
+		if ( sender instanceof Player) {
+			UUID player = ( (Player) sender ).getUniqueId();
+			VeinMiner.settings.putIfAbsent( player, new VeinMinerSettings() );
+			VeinMinerSettings settings = VeinMiner.settings.get(player);
+			settings.setActive(! settings.isActive() );
+			VeinMiner.settings.put( player, settings );
+		}
+		return true;
 	}
 
 	@Override
--- src/main/java/com/enderzombi102/randomutils/modules/veinminer/VeinMiner.java
+++ src/main/java/com/enderzombi102/randomutils/modules/veinminer/VeinMiner.java
@@ -1,18 +1,22 @@
 package com.enderzombi102.randomutils.modules.veinminer;
 
 import com.enderzombi102.randomutils.modules.ModuleBase;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.HashMap;
 import java.util.UUID;
 
 public class VeinMiner extends ModuleBase {
 
-	private HashMap<UUID, VeinMinerSettings> settings = new HashMap<>();
-	private VeinMinerListener listener;
+	public static HashMap<UUID, VeinMinerSettings> settings = new HashMap<>();
+	private final VeinMinerListener listener;
 
 	public VeinMiner() {
 		this.listener = new VeinMinerListener();
@@ -23,10 +27,25 @@ public class VeinMiner extends ModuleBase {
 		HandlerList.unregisterAll(this.listener);
 	}
 
+	private void breakConnected(Block block, ItemStack tool) {
+		for ( BlockFace face : BlockFace.values() ) {
+			Block blockFound = block.getRelative(face);
+			if ( blockFound.getType() == block.getType() ) {
+				breakConnected(blockFound, tool);
+				block.breakNaturally(tool);
+			}
+		}
+	}
+
 	private class VeinMinerListener implements Listener {
 		@EventHandler
 		public void onBlockBreak(BlockBreakEvent evt) {
-
+			Player player = evt.getPlayer();
+			if ( player.isSneaking() ) {
+				if ( settings.getOrDefault( player.getUniqueId(), new VeinMinerSettings() ).isActive() ) {
+					breakConnected( evt.getBlock(), player.getInventory().getItemInMainHand() );
+				}
+			}
 		}
 	}
 }
